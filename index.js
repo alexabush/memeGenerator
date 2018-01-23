3698c2d5c76dbdc2dc6f23d03cef5144bf5a1697
Alex Bush <alexabush7@gmail.com>
2018-01-23 10:13:34 -0800

    Submission and appending functionality working, currently no styling for submitted content

--- index.js
+++ index.js
@@ -4,21 +4,51 @@ const displayArea = document.getElementById('displayArea');
 
 form.addEventListener('submit', function(e) {
   // debugger;
-  const val1 = inputs[0].value;
-  const val2 = inputs[1].value;
-  const val3 = inputs[2].value;
-  const newSection = document.createElement('section');
-  const newImg = document.createElement('img');
-  //add text to image
+  const text1 = inputs[0].value;
+  const text2 = inputs[1].value;
+  const text3 = inputs[2].value;
+  
+  const section = document.createElement('section');
+  const image = document.createElement('img');
+  const topText = document.createElement('p');
+  const bottomText = document.createElement('p');
   const newBtn = document.createElement('button');
-  //set btn txt
-  newSection.append(newImg);
-  newSection.append(newBtn);
-  displayArea.append(newSection);
+  
+  section.classList.add('imgSection');
+  image.src = text1;
+  topText.textContent = text2;
+  bottomText.textContent = text3;
+  newBtn.textContent = "Delete";
+
+  section.append(image)
+  section.append(topText);
+  section.append(bottomText)
+  section.append(newBtn);
+
+  displayArea.append(section);
 });
 
 displayArea.addEventListener('click', function(e) {
-    // if (e.target type equals button) {
-        // element deletes parent element and itself
-    // }
+  // if (e.target type equals button) {
+  // element deletes parent element and itself
+  // }
 })
+
+// debugger;
+// const sectionList = document.querySelectorAll('.imgSection');
+// const section = sectionList[0];
+// const trees = document.querySelector('img');
+// const topText = document.createElement('p');
+// topText.textContent = "Top";
+// const bottomText = document.createElement('p');
+// bottomText.textContent = "Bottom";
+// section.append(topText);
+// section.append(bottomText)
+// const newBtn = document.createElement('button');
+// newBtn.textContent = "Delete";
+// section.append(newBtn);
+
+// trees.addEventListener('click', function(e) {
+
+//   console.log('yay')
+// })
