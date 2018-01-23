b0b26cef53d43d2546205ad7c36ff0f7f5c9b889
Alex Bush <alexabush7@gmail.com>
2018-01-23 11:21:03 -0800

    styled img/text section that is added to DOM

--- index.js
+++ index.js
@@ -7,18 +7,24 @@ form.addEventListener('submit', function(e) {
   const text1 = inputs[0].value;
   const text2 = inputs[1].value;
   const text3 = inputs[2].value;
-  
+
   const section = document.createElement('section');
   const image = document.createElement('img');
   const topText = document.createElement('p');
   const bottomText = document.createElement('p');
   const newBtn = document.createElement('button');
-  
+
   section.classList.add('imgSection');
+  topText.classList.add('topText');
+  topText.style["background-color"] = "orange;"
+  bottomText.classList.add('bottomText')
+
   image.src = text1;
   topText.textContent = text2;
   bottomText.textContent = text3;
-  newBtn.textContent = "Delete";
+  newBtn.textContent = "X";
+
+  setStyle(section, topText, bottomText, newBtn);
 
   section.append(image)
   section.append(topText);
@@ -29,26 +35,39 @@ form.addEventListener('submit', function(e) {
 });
 
 displayArea.addEventListener('click', function(e) {
-  // if (e.target type equals button) {
-  // element deletes parent element and itself
-  // }
+  if (e.target.tagName === "BUTTON") {
+    e.target.parentElement.remove();
+  }
 })
 
-// debugger;
-// const sectionList = document.querySelectorAll('.imgSection');
-// const section = sectionList[0];
-// const trees = document.querySelector('img');
-// const topText = document.createElement('p');
-// topText.textContent = "Top";
-// const bottomText = document.createElement('p');
-// bottomText.textContent = "Bottom";
-// section.append(topText);
-// section.append(bottomText)
-// const newBtn = document.createElement('button');
-// newBtn.textContent = "Delete";
-// section.append(newBtn);
 
-// trees.addEventListener('click', function(e) {
+function setStyle(section, topText, bottomText, button) {
+  setPStyle(topText)
+  setPStyle(bottomText)
 
-//   console.log('yay')
-// })
+  section.style["position: relative"];
+  section.style["color: white"];
+  section.style["display: flex"];
+  section.style["justify-content: center"]
+
+  topText.style["position: absolute"];
+  topText.style["top: 10px"]
+  topText.style["left: 30%"]
+  topText.style["color: orange"];
+
+  bottomText.style["position: absolute"]
+  bottomText.style["bottom: 20px"]
+  bottomText.style["left: 30%"]
+  bottomText.style["color: purple"]
+
+  // button.style["position: block"]
+  // // button.style["top: 0px"]
+  // // button.style["right: 0px"]
+  // button.innerText = "Button"
+
+  function setPStyle(p) {
+    p.style["font-size: 42px"];
+    p.style["weight: bold"]
+  }
+
+}
