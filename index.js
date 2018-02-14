ed480b5be29a3deb724f340ea993065f76358c2a
Alex Bush <alexabush7@gmail.com>
2018-02-14 10:48:19 -0800

    finished changes to memeGenerator

--- index.js
+++ index.js
@@ -2,66 +2,37 @@ document.addEventListener('DOMContentLoaded', function() {
   const form = document.querySelector('form');
   const inputs = document.querySelectorAll('input');
   const displayArea = document.getElementById('displayArea');
-  addRickAndMorty();
 
+  const puppyUrl = 'https://fthmb.tqn.com/yaoer7uekYUl9PP6lIYiI0IEZnk=/1500x998/filters:fill(auto,1)/Biting-UweKrejci-Getty-56a7a3415f9b58b7d0ec54d4.jpg'
+  addContent("https://i.ytimg.com/vi/0Y4mx6KJAIk/maxresdefault.jpg", "I'm Mr. Meeseeks", "Look At Me!");
+  addContent(puppyUrl, "Raw")
 
   form.addEventListener('submit', function(e) {
+    addContent(inputs[0].value, inputs[1].value, inputs[2].value);
+    form.reset();
+  });
+
+  function addContent(imgSrc, topText, bottomText) {
     const section = document.createElement('section');
     const image = document.createElement('img');
-    const topText = document.createElement('p');
-    const bottomText = document.createElement('p');
+    const topP = document.createElement('p');
+    const bottomP = document.createElement('p');
     const newBtn = document.createElement('button');
+    section.classList.add('imgSection');
+    topP.classList.add('topText');
+    bottomP.classList.add('bottomText')
 
-    addContent();
-
-    section.append(image, topText, bottomText, newBtn);
+    image.src = imgSrc;
+    topP.textContent = topText;
+    bottomP.textContent = bottomText;
+    newBtn.textContent = "X";
+    section.append(image, topP, bottomP, newBtn);
     displayArea.append(section);
-
-    form.reset();
-
-    function addContent() {
-      section.classList.add('imgSection');
-      topText.classList.add('topText');
-      topText.style["background-color"] = "orange;"
-      bottomText.classList.add('bottomText')
-
-      image.src = inputs[0].value;
-      topText.textContent = inputs[1].value;
-      bottomText.textContent = inputs[2].value;
-      newBtn.textContent = "X";
-    }
-  });
+  }
 
   displayArea.addEventListener('click', function(e) {
     if (e.target.tagName === "BUTTON") {
       e.target.parentElement.remove();
     }
   });
-
-  function addRickAndMorty() {
-    const section = document.createElement('section');
-    const image = document.createElement('img');
-    const topText = document.createElement('p');
-    const bottomText = document.createElement('p');
-    const newBtn = document.createElement('button');
-
-    addContent();
-
-    section.append(image, topText, bottomText, newBtn);
-    displayArea.append(section);
-
-    form.reset();
-
-    function addContent() {
-      section.classList.add('imgSection');
-      topText.classList.add('topText');
-      topText.style["background-color"] = "orange;"
-      bottomText.classList.add('bottomText')
-
-      image.src = "https://i.ytimg.com/vi/0Y4mx6KJAIk/maxresdefault.jpg";
-      topText.textContent = "I'm Mr. Meeseeks";
-      bottomText.textContent = "Look At Me!";
-      newBtn.textContent = "X";
-    }
-  }
 });
